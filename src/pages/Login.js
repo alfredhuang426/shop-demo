7606cbb0e15b2519577b10b318a48f0f2399c208
Alfred Huang <whatever0426.am03@gmail.com>
2023-12-31 01:15:38 +0800

    login page

---
+++ src/pages/Login.js
@@ -0,0 +1,67 @@
+import axios from "axios";
+import { useState } from "react";
+
+function Login() {
+  const [data, setData] = useState({
+    username: "",
+    password: "",
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setData({ ...data, [name]: value });
+  };
+
+  const submit = async (e) => {
+    const result = await axios.post("/v2/admin/signin", data);
+    console.log(result);
+    const { token, expired } = result.data;
+    console.log(result.data);
+    document.cookie = `hexToken=${token}; expires=${new Date(expired)}`;
+  };
+
+  return (
+    <div className="container py-5">
+      <div className="row justify-content-center">
+        <div className="col-md-6">
+          <h2>登入帳號</h2>
+
+          <div className="alert alert-danger" role="alert">
+            錯誤訊息
+          </div>
+          <div className="mb-2">
+            <label htmlFor="email" className="form-label w-100">
+              Email
+              <input
+                id="email"
+                className="form-control"
+                name="username"
+                type="email"
+                placeholder="Email Address"
+                onChange={handleChange}
+              />
+            </label>
+          </div>
+          <div className="mb-2">
+            <label htmlFor="password" className="form-label w-100">
+              密碼
+              <input
+                type="password"
+                className="form-control"
+                name="password"
+                id="password"
+                placeholder="name@example.com"
+                onChange={handleChange}
+              />
+            </label>
+          </div>
+          <button type="button" onClick={submit} className="btn btn-primary">
+            登入
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Login;
